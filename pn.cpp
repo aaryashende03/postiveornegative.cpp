4dbddd2d597bc2fbf8be28d697501178de31ffb8
aaryashende03 <168561498+aaryashende03@users.noreply.github.com>
2024-08-03 15:31:21 +0530

    Add files via upload

---
+++ pn.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"Enter the number ="<<endl;
+	cin>>number;
+	
+	if(number>=0)
+	{
+		cout<<"The number is postive"<<endl;
+	}
+	
+	else
+	{
+		cout<<"The number is negative"<<endl;
+	}
+	
+	return 0;
+}
+	
